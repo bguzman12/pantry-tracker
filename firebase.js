ed5ae03ba34366d8385084ebba90c148273e2b5c
braianguzman <braianaliskair@gmail.com>
2024-08-10 21:20:16 -0400

    Inventory tracker app is coming together

---
+++ firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+const firebaseConfig = {
+ apiKey: "AIzaSyBg4uJHfHhhJP1TqqLobElDju8G6uxP2Hk",
+ authDomain: "pantry-tracker-e3055.firebaseapp.com",
+ projectId: "pantry-tracker-e3055",
+ storageBucket: "pantry-tracker-e3055.appspot.com",
+ messagingSenderId: "113180007837",
+ appId: "1:113180007837:web:52d3d7e3f3b359b4172374"
+ };
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
+export { firestore };
