8cbc4e36c4a690a6e415e673cadc3a4718aff5d2
braianguzman <braianaliskair@gmail.com>
2024-08-11 12:55:41 -0400

    Correctly synced to firebase to display inventory

--- app/page.js
+++ app/page.js
@@ -33,7 +33,6 @@ export default function Home() {
   const [inventory, setInventory] = useState([])
   const [open, setOpen] = useState(false)
   const [itemName, setItemName] = useState('')
-
   const updateInventory = async () => {
     const snapshot = query(collection(firestore, 'inventory'))
     const docs = await getDocs(snapshot)
@@ -41,9 +40,9 @@ export default function Home() {
     docs.forEach((doc) => {
       inventoryList.push({ name: doc.id, ...doc.data() })
     })
+    console.log(inventoryList)
     setInventory(inventoryList)
   }
-  
   useEffect(() => {
     updateInventory()
   }, [])
--- firebase.js
+++ firebase.js
@@ -1,13 +1,14 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 const firebaseConfig = {
- apiKey: "AIzaSyBg4uJHfHhhJP1TqqLobElDju8G6uxP2Hk",
- authDomain: "pantry-tracker-e3055.firebaseapp.com",
- projectId: "pantry-tracker-e3055",
- storageBucket: "pantry-tracker-e3055.appspot.com",
- messagingSenderId: "113180007837",
- appId: "1:113180007837:web:52d3d7e3f3b359b4172374"
- };
+    apiKey: "AIzaSyBg4uJHfHhhJP1TqqLobElDju8G6uxP2Hk",
+    authDomain: "pantry-tracker-e3055.firebaseapp.com",
+    projectId: "pantry-tracker-e3055",
+    storageBucket: "pantry-tracker-e3055.appspot.com",
+    messagingSenderId: "113180007837",
+    appId: "1:113180007837:web:52d3d7e3f3b359b4172374",
+    measurementId: "G-57TM0YBTJ7"
+  };
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
 export { firestore };
